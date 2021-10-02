91d44333f38ea502bd20ec6cd1119614bcf668a7
hzegai <houari.zegai@smartest.dz>
2021-10-02 23:16:28 +0200

    Upload project

---
+++ src/main/java/com/houarizegai/openidmock/OpenidMockApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.openidmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenidMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenidMockApplication.class, args);
+    }
+
+}
